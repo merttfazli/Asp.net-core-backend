4fd00f1b322fcd4fe0e99076a2f2b740f0354213
Mert Fazlı Saylak <76404240+merttfazli@users.noreply.github.com>
2024-03-13 17:33:48 +0300

    13.03.2024 -

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,5 @@
-﻿using Entitites.Concrete;
+﻿using Core.DataAccess;
+using Entitites.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,6 @@ namespace DataAccess.Abstract
 {
     public interface IProductDal:IEntityRepository<Product>
     {
+
     }
 }
