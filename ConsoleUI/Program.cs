c0af3c9762766b891a95e0f7215d3c480113c8dc
Mert Fazlı Saylak <76404240+merttfazli@users.noreply.github.com>
2024-06-24 17:55:29 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -29,8 +29,8 @@ namespace ConsoleUI
 
         private static void ProductTest()
         {
-            ProductManager productManager = new ProductManager(new EfProductDal(),new CategoryManager(new EfCategoryDal()));
-            var details=productManager.GetProductDetailDtos();
+            ProductManager productManager = new ProductManager(new EfProductDal(), new CategoryManager(new EfCategoryDal()));
+            var details = productManager.GetProductDetailDtos();
 
             if (details.Success)
             {
@@ -42,7 +42,7 @@ namespace ConsoleUI
             else
             {
                 Console.WriteLine(details.Message);
-            }           
+            }
             Console.ReadLine();
         }
     }
